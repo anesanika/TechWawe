045702ef88e56253514dd51178115700eabbde75
anesanika <nikaanesiani06@gmail.com>
2025-03-28 03:54:52 -0700

    fix sec

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,13 @@
 import Allproduct from "../components/allproduct/Allproduct";
 import { Providers } from "../providers";
+import { Suspense } from "react";
 
 export default async function Product() {
   return (
     <Providers>
-      <Allproduct />
+      <Suspense fallback={<>loading..</>}>
+        <Allproduct />
+      </Suspense>
     </Providers>
   );
 }
